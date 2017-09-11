1c45f7224c4b7a278b98333bbf5043b8259852a0
Bas <basjansen14@outlook.com>
2017-09-11 15:36:51 +0200

    Update Casino.java

--- src/blackjack/Casino.java
+++ src/blackjack/Casino.java
@@ -21,69 +21,69 @@ public class Casino {
 	@SuppressWarnings("resource")
 	public void startBlackjack() {
 		System.out.println("Welkom in het iCasino! We gaan fijn Blackjack spelen!!\n");
-        System.out.println("Commando's");
-        System.out.println("----------");
-        System.out.println("p = passen");
-        System.out.println("d = draaien");
-        System.out.println("2 = inzet verdubbelen");
+                System.out.println("Commando's");
+                System.out.println("----------");
+                System.out.println("p = passen");
+                System.out.println("d = draaien");
+                System.out.println("2 = inzet verdubbelen");
         
-        try {
-        	Scanner scan = new Scanner(System.in);
-            System.out.print("\nWat is je naam? ");
-        	player.setName(scan.nextLine());
-        	
-        	System.out.println("Welkom, " + player.getName() + ". Je startkapitaal is \u20ac 1000,-");
- 
-        	while(aantalHanden == 0) {
-        		try {
-        			Scanner c = new Scanner(System.in);
-                	System.out.print("Met hoeveel handen wil je spelen (1-5)? ");
-        			
-        			aantalHanden = c.nextInt();
-        			maxInzet = START_KAPITAAL / aantalHanden;
-        			
-        			if (aantalHanden < 0 || aantalHanden > 5) {
-        				aantalHanden = 0;
-        				System.out.println("Dat aantal ligt niet tussen 1 en 5");
-        				continue;
-        			}
-        			
-        			break;
-        		}
-        		catch(Exception e) {
-        			System.out.println("Dat begrijp ik niet.");
-        		}
-        	}
-        	      	
-        	while(inzet == 0) {
-        		try {
-        			Scanner c = new Scanner(System.in);
-		        	System.out.print("Met welke inzet wil je spelen (1-" + maxInzet + ")");
-		        	inzet = c.nextInt();
-		        	
-		        	if (inzet > maxInzet || inzet < 0) {
-		        		System.out.println("Dat aantal ligt niet tussen 1 en " + maxInzet);
-		        		inzet = 0;
-		        		continue;
-		        	}
-		        	
-		        	System.out.println();
-		        	
-		        	player.setInzet(inzet);
-		        	
-		        	break;
-        		}
-        		catch(Exception e) {
-        			System.out.println("Dat begrijp ik niet.");
-        		}
-        	}
-        	
-        	// Start een spelletje Blackjack
-            new Blackjack(aantalHanden, player);
-        }
-        catch(Exception e) {
-        	System.out.println("Er is iets fout gegaan!");
-        	e.printStackTrace();
-        }
+                try {
+			Scanner scan = new Scanner(System.in);
+			System.out.print("\nWat is je naam? ");
+			player.setName(scan.nextLine());
+
+			System.out.println("Welkom, " + player.getName() + ". Je startkapitaal is \u20ac 1000,-");
+
+			while(aantalHanden == 0) {
+				try {
+					Scanner c = new Scanner(System.in);
+				System.out.print("Met hoeveel handen wil je spelen (1-5)? ");
+
+					aantalHanden = c.nextInt();
+					maxInzet = START_KAPITAAL / aantalHanden;
+
+					if (aantalHanden < 0 || aantalHanden > 5) {
+						aantalHanden = 0;
+						System.out.println("Dat aantal ligt niet tussen 1 en 5");
+						continue;
+					}
+
+					break;
+				}
+				catch(Exception e) {
+					System.out.println("Dat begrijp ik niet.");
+				}
+			}
+
+			while(inzet == 0) {
+				try {
+					Scanner c = new Scanner(System.in);
+					System.out.print("Met welke inzet wil je spelen (1-" + maxInzet + ")");
+					inzet = c.nextInt();
+
+					if (inzet > maxInzet || inzet < 0) {
+						System.out.println("Dat aantal ligt niet tussen 1 en " + maxInzet);
+						inzet = 0;
+						continue;
+					}
+
+					System.out.println();
+
+					player.setInzet(inzet);
+
+					break;
+				}
+				catch(Exception e) {
+					System.out.println("Dat begrijp ik niet.");
+				}
+			}
+
+			// Start een spelletje Blackjack
+		    new Blackjack(aantalHanden, player);
+                }
+                catch(Exception e) {
+        		System.out.println("Er is iets fout gegaan!");
+        		e.printStackTrace();
+                }
 	}
 }
